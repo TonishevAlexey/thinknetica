4c47e207ec65735820d01cd81f161b1d1f0bd4ca
aleksey <atonishev@phoenixit.ru>
2021-07-25 16:52:49 +0300

    дз 4

--- home4/station.rb
+++ home4/station.rb
@@ -27,6 +27,12 @@ class Station
 
   # Может возвращать список поездов на станции по типу (см. ниже): кол-во грузовых, пассажирских
   def trains_type(type)
+    render_trains(type)
+  end
+
+  private
+
+  def render_trains(type)
     n = []
     @trains.each { |train| n << @trains if train.type == type }
     n
