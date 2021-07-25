1de260dc4713e77ada70930246186add3e14561b
aleksey <atonishev@phoenixit.ru>
2021-07-25 14:06:28 +0300

    дз 4

--- home4/main.rb
+++ home4/main.rb
@@ -7,98 +7,165 @@ require_relative 'route'
 require_relative 'station'
 
 class Main
-  @n = 0
-  @station = []
-  loop do
-    print "Введите название станции: "
-    name_station = gets.chomp
-    @station << Station.new(name_station)
-    @n += 1
-    print "Для окончаеия создания станций введите(если их больше двух)stop если хотите продолжить нажмите enter:"
-    break if gets.chomp == 'stop' && @n >= 2
-  end
-  @trains = []
-  loop do
-    print "Введите номер поезда: "
-    number_train = gets.chomp
-    print "Введите тип поезда(пассажирский или грузавой): "
-    type_train = gets.chomp
-    if type_train == 'пассажирский'
-      @trains << CargoTrain.new(number_train)
-    else
-      @trains << PassengerTrain.new(number_train)
-    end
-    print "Для окончаеия создания поездов введите stop если хотите продолжить нажмите enter:"
-    break if gets.chomp == 'stop'
+  @@station = []
+  @@trains = []
+  @@route = 0
+
+  def create_station
+    create_s
   end
 
-  @n_s = 0
-  puts "Маршрут.Выберете начальную станцию"
-  @station.each do |s|
-    puts "Для выбора: #{s.name} введите #{@n_s}"
-    @n_s += 1
+  def create_train
+    create_t
   end
-  start_station = gets.chomp.to_i
-  @n_s = 0
-  puts "Маршрут.Выберете конечную станцию станцию:"
-  @station.each do |s|
-    puts "Для выбора: #{s.name} введите #{@n_s}" if start_station != @n_s
-    @n_s += 1
+
+  def create_route
+    create_r
   end
-  end_station = gets.chomp.to_i
-  @route = Route.new(@station[start_station], @station[end_station])
-  @n_s = 0
-  puts "Маршрут.Выберете какую станцию добавить:"
-  @station.each do |s|
-    puts "Для выбора #{s.name} введите #{@n_s}" if start_station != @n_s && end_station != @n_s
-    @n_s += 1
+
+  def add_route
+    add_r
   end
-  add_station = gets.chomp.to_i
-  @route.add_station(@station[add_station])
-  @n_t = 0
-  puts "Выберете поезд к котрому хотите добавить маршрут:"
-  @trains.each do |s|
-    puts "Для выбора #{s.number} введите #{@n_t}"
-    @n_t += 1
+
+  def add_car
+    add_c
   end
-  trains = gets.chomp.to_i
-  @trains[trains].assign_route(@route)
-  @n_t = 0
-  puts "Выберете поезд к коророму хотите добавить вагон:"
-  @trains.each do |s|
-    puts "Для выбора #{s.number} введите #{@n_t}"
-    @n_t += 1
+
+  def delete_car
+    delete_c
   end
-  trains = gets.chomp.to_i
-  if 'PassengerCars' == trains.class.to_s
-    @trains[trains].add_car
-  elsif 'CargoCars' == trains.class.to_s
-    @trains[trains].add_car
+
+  def go_next
+    run_car
   end
-  @n_t = 0
-  puts "Выберете поезд у которо хотит удалить вагон:"
-  @trains.each do |s|
-    puts "Для выбора #{s.number} введите #{@n_t}"
-    @n_t += 1
+
+  def go_back
+    run_back
   end
-  trains = gets.chomp.to_i
-  @trains[trains].delete_car
-  @n_t = 0
-  puts "Выберете поезд который хотите отправить на следующую станцию:"
-  @trains.each do |s|
-    puts "Для выбора #{s.number} введите #{@n_t}"
-    @n_t += 1
+
+  private
+
+  def run_back
+    @n_t = 0
+    puts "Выберете поезд который хотите отправить на предыдущую станцию:"
+    @@trains.each do |s|
+      puts "Для выбора #{s.number} введите #{@n_t}"
+      @n_t += 1
+    end
+    station = @@trains[gets.chomp.to_i]
+    station.go_back
   end
-  station = @trains[gets.chomp.to_i]
-  station.go_next
-  @n_t = 0
-  puts "Выберете поезд который хотите отправить на предыдущую станцию:"
-  @trains.each do |s|
-    puts "Для выбора #{s.number} введите #{@n_t}"
-    @n_t += 1
+
+  def run_car
+    @n_t = 0
+    puts "Выберете поезд который хотите отправить на следующую станцию:"
+    @@trains.each do |s|
+      puts "Для выбора #{s.number} введите #{@n_t}"
+      @n_t += 1
+    end
+    station = @@trains[gets.chomp.to_i]
+    station.go_next
   end
-  station = @trains[gets.chomp.to_i]
-  station.go_back
+
+  def delete_c
+    @n_t = 0
+    puts "Выберете поезд у которо хотит удалить вагон:"
+    @@trains.each do |s|
+      puts "Для выбора #{s.number} введите #{@n_t}"
+      @n_t += 1
+    end
+    trains = gets.chomp.to_i
+    @@trains[trains].delete_car
+  end
+
+  def create_s
+    @n = 0
+    loop do
+      print "Введите название станции(Для окончаеия создания станций введите(если их больше двух)stop):"
+      name_station = gets.chomp
+      @n += 1
+      break if name_station == 'stop' && @n >= 2
+      @@station << Station.new(name_station)
+    end
+  end
+
+  def create_t
+    loop do
+      print "Введите номер поезда(Для окончаеия создания поездов введите stop): "
+      number_train = gets.chomp
+      break if number_train == 'stop'
+      print "Введите тип поезда(пассажирский или грузавой):"
+      type_train = gets.chomp
+      if type_train == 'пассажирский'
+        @@trains << CargoTrain.new(number_train)
+      else
+        @@trains << PassengerTrain.new(number_train)
+      end
+    end
+  end
+
+  def create_r
+    @n_s = 0
+    puts "Маршрут.Выберете начальную станцию"
+    @@station.each do |s|
+      puts "Для выбора: #{s.name} введите #{@n_s}"
+      @n_s += 1
+    end
+    start_station = gets.chomp.to_i
+    @n_s = 0
+    puts "Маршрут.Выберете конечную станцию станцию:"
+    @@station.each do |s|
+      puts "Для выбора: #{s.name} введите #{@n_s}" if start_station != @n_s
+      @n_s += 1
+    end
+    end_station = gets.chomp.to_i
+    @@route = Route.new(@@station[start_station], @@station[end_station])
+    @n_s = 0
+    puts "Маршрут.Выберете какую станцию добавить:"
+    @@station.each do |s|
+      puts "Для выбора #{s.name} введите #{@n_s}" if start_station != @n_s && end_station != @n_s
+      @n_s += 1
+    end
+    add_station = gets.chomp.to_i
+    @@route.add_station(@@station[add_station])
+  end
+
+  def add_r
+    @n_t = 0
+    puts "Выберете поезд к котрому хотите добавить маршрут:"
+    @@trains.each do |s|
+      puts "Для выбора #{s.number} введите #{@n_t}"
+      @n_t += 1
+    end
+    trains = gets.chomp.to_i
+    @@trains[trains].assign_route(@@route)
+
+  end
+
+  def add_c
+    trains = gets.chomp.to_i
+    @@trains[trains].assign_route(@@route)
+    @n_t = 0
+    puts "Выберете поезд к коророму хотите добавить вагон:"
+    @@trains.each do |s|
+      puts "Для выбора #{s.number} введите #{@n_t}"
+      @n_t += 1
+    end
+    trains = gets.chomp.to_i
+    if 'PassengerCars' == trains.class.to_s
+      @@trains[trains].add_car
+    elsif 'CargoCars' == trains.class.to_s
+      @@trains[trains].add_car
+    end
+  end
 end
 
-Main.new
+m = Main.new
+m.create_station
+m.create_train
+m.create_route
+m.add_route
+m.add_car
+m.delete_car
+m.go_next
+m.go_back
